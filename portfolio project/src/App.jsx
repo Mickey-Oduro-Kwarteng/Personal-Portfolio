94c3b37bb125fe2c3a66be69bd969ca06800844b
Mickey-Oduro-Kwarteng <odurokwartengmickey@gmail.com>
2025-09-03 03:13:10 +0000

    added projects component

--- portfolio project/src/App.jsx
+++ portfolio project/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/hero/hero";
 import About from "./components/About/About";
 import Services from "./components/Services/Services";
+import Projects from "./components/Projects/Projects";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <About />
       <Services />
+      <Projects />
     </div>
   );
 };
